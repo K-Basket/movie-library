89e94ddb21c480bf3f1be5f6493857b04aadc715
K-Basket <koshykkv@gmail.com>
2023-11-14 16:47:52 +0200

    Update Header
    If the Home page - there is a form
    If not Home page - no form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,7 @@ import {
   SearchResult,
   WrappLinkLogo,
 } from './Header.styled';
+import { useLocation } from 'react-router-dom';
 
 export const Header = () => {
   const handleMovieSearch = evt => {
@@ -16,27 +17,32 @@ export const Header = () => {
     console.log('Button Movie Search');
   };
 
+  const { pathname } = useLocation();
+  const isPathHome = pathname === '/';
+
   return (
     <HeaderWrap>
       <Nav>
         <NavLinkStyled to="/">Home</NavLinkStyled>
         <NavLinkStyled to="/movies">My Library</NavLinkStyled>
 
-        <form>
-          <Input
-            type="text"
-            name="searchQuery"
-            autoComplete="off"
-            autoFocus
-            placeholder="Movie search"
-          />
+        {isPathHome && (
+          <form>
+            <Input
+              type="text"
+              name="searchQuery"
+              autoComplete="off"
+              autoFocus
+              placeholder="Movie search"
+            />
 
-          <IconButton type="submit" onClick={handleMovieSearch}>
-            <svg width="16px" height="16px">
-              <use href={`${sprite}#icon-search`}></use>
-            </svg>
-          </IconButton>
-        </form>
+            <IconButton type="submit" onClick={handleMovieSearch}>
+              <svg width="16px" height="16px">
+                <use href={`${sprite}#icon-search`}></use>
+              </svg>
+            </IconButton>
+          </form>
+        )}
       </Nav>
 
       {/* Якщо не знайдено - true, в прротилежному - false */}
