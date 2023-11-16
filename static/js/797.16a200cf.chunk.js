6372932a3b731e697790350f8db6831cdcd2aeed
K-Basket <114851751+K-Basket@users.noreply.github.com>
2023-11-16 14:47:35 +0000

    Deploying to gh-pages from @ K-Basket/movie-library@ceeff7079dfbe2f1077cd3dc1cfe57454bb5db58 🚀

---
+++ static/js/797.16a200cf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmovie_library=self.webpackChunkmovie_library||[]).push([[797],{4797:function(e,r,t){t.r(r);var n=t(7689),i=t(1087),s=t(184);r.default=function(){var e=(0,n.TH)();return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)("h1",{children:"Movies pages"}),(0,s.jsx)("button",{type:"button",children:"watched"}),(0,s.jsx)("button",{type:"button",children:"queue"}),(0,s.jsx)(i.rU,{to:"id",state:{from:e},children:(0,s.jsx)("h3",{style:{color:"green"},children:"Movie Card (id)"})})]})}}}]);
+//# sourceMappingURL=797.16a200cf.chunk.js.map
