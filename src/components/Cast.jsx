5ce108a8e3b241b16f00c6d6a53515beb8ae85ab
K-Basket <koshykkv@gmail.com>
2023-12-03 21:34:38 +0200

    add style Cast.jsx

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,5 +1,7 @@
 import { Link, useLocation, useParams } from 'react-router-dom';
 import { useGetCastMovieQuery } from 'redux/moviesSlice';
+import { CardCast } from './Cast.styled';
+import { Item } from 'pages/MovieDetailsPage.styled';
 
 export const Cast = () => {
   const { id: movieId } = useParams();
@@ -14,30 +16,21 @@ export const Cast = () => {
   const { cast } = data;
 
   return (
-    <ul
-      style={{
-        margin: '10px 0',
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-      }}
-    >
+    <CardCast>
       {cast.map(({ id, character, name, popularity, profile_path }) => (
-        <li key={id}>
-          {
-            <Link to={`/actor?id=${id}`} state={{ from: location }}>
-              <h2>character: {character}</h2>
-              <p>Name cast: {name}</p>
-              <p>Popularity: {popularity}</p>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${profile_path}`}
-                alt={character}
-                width="150"
-              />
-            </Link>
-          }
-        </li>
+        <Item key={id}>
+          <Link to={`/actor?id=${id}`} state={{ from: location }}>
+            <h2>character: {character}</h2>
+            <p>Name cast: {name}</p>
+            <p>Popularity: {popularity}</p>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${profile_path}`}
+              alt={character}
+              width="150"
+            />
+          </Link>
+        </Item>
       ))}
-    </ul>
+    </CardCast>
   );
 };
---
+++ src/components/Cast.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const CardCast = styled.ul``;
+
+export const Item = styled.li``;
+
+export const Card = styled.article``;
+
+export const CardImg = styled.div``;
+
+export const CardContent = styled.div``;
